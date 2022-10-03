f22ecafbf668fc621bd690e126e4ac427b832c18
TempSFAcc <114776994+TempSFAcc@users.noreply.github.com>
2022-10-03 13:02:27 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,3 +65,10 @@ if streamlit.button('Add a Fruit to the List'):
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
 streamlit.stop()
+
+streamlit.header("View Our Fruit List - Add Your Favourites!")
+if streamlit.button('Get Fruit List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    my_cnx.close()
+    streamlit.dataframe(my_data_rows)
